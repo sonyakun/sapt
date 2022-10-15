3507b6f97b2dae92abbb44457aad89ed3d3ca8d5
sonyakun <90973770+sonyakun@users.noreply.github.com>
2022-10-15 18:52:21 +0900

    Update setup.py

--- sapt/setup.py
+++ sapt/setup.py
@@ -10,6 +10,6 @@ setup(
     install_requires=install_requirements,
     entry_points={
         "console_scripts": [
-            "sapt insrm=aptUtil:insrm",
+            "sapt insrm=sapt.aptUtil:insrm",
         ]
 )
